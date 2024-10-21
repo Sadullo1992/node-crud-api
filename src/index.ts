bcfe593252b1716b632cc460593e44b263eede36
Sadullo <sadulloburiyev1992@gmail.com>
2024-10-21 22:18:58 +0500

    feat: add routes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
-import { createServer } from 'http';
+import { createServer, IncomingMessage, ServerResponse } from 'http';
 import 'dotenv/config';
+import { sendMessage } from './utils/messages';
+import { MSG_API_ROUTE_404 } from './constants';
+import { StatusCodes } from './types';
 
 const PORT = process.env.PORT || 3000;
+const PATTERN = /\/api\/users\/\w+/;
+
+const listener = async (req: IncomingMessage, res: ServerResponse) => {
+  try {
+    if (req.url === '/api/users' && req.method === 'GET') {
+      // getUsers(res);
+    } else if (req.url?.match(PATTERN) && req.method === 'GET') {
+      // getUser(req, res);
+    } else if (req.url === '/api/users' && req.method === 'POST') {
+      // createUser(req, res);
+    } else if (req.url?.match(PATTERN) && req.method === 'PUT') {
+      // updateUser(req, res);
+    } else if (req.url?.match(PATTERN) && req.method === 'DELETE') {
+      // deleteUser(req, res);
+    } else
+      sendMessage(res, StatusCodes.NOT_FOUND, { message: MSG_API_ROUTE_404 });
+  } catch {
+    sendMessage(res, StatusCodes.INTERNAL_SERVER_ERROR, {
+      message: 'Internal server error!',
+    });
+  }
+};
+
 const init = () => {
-  const server = createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('okay');
-  });
+  const server = createServer(listener);
 
   server.listen(PORT, () => {
     console.log(`Server is running port: ${PORT}`);
---
+++ src/utils/messages.ts
@@ -0,0 +1,8 @@
+import { ServerResponse } from 'http';
+
+const sendMessage = (res: ServerResponse, code: number, data: unknown) => {
+  res.writeHead(code, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(data));
+};
+
+export { sendMessage };
